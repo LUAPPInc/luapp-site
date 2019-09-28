7bc049a53d4bb0ac0c57981f002bbf3b3ddef361
Vinícius Marcili <vinipetter@gmail.com>
2019-09-28 19:11:13 -0300

    About Page

--- src/template_parts/index/about.tsx
+++ src/template_parts/index/about.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import luappSvg from '../../images/luapp_purple.svg'
+import phoneSvg from '../../images/phone.svg'
 
 interface Props {}
 
@@ -8,7 +11,51 @@ const About = ({}: Props) => {
       className='about'
       id='#sobre'
     >
-
+      <p
+        className='app'
+      >
+        _Desenvolvimento de aplicativos
+      </p>
+      <Container>
+        <div className='box'>
+          <div className='wrapper'>
+            <div
+              className='texts'
+            >
+              <figure>
+                <img
+                  title='Lunnar Applications'
+                  alt='Lunnar Applications'
+                  className='img-fluid'
+                  src={luappSvg}
+                />
+              </figure>
+              <p>
+                Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai. Só digo uma coisa, Domingo ela não vai! Danadaa!! Vem minha odalisca, agora faz essa cobra coral subir!!! Pau que nasce torto, Nunca se endireita. Tchannn!! Tchannn!! Tu du du pááá! Eu gostchu muitchu, heinn! danadinha! Mainhaa! Agora use meu lorem ipsum ordinária!!! Olha o quibeee! rema, rema, ordinária!.
+                <br />
+                <br />
+                Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai. Só digo uma coisa, Domingo ela não vai! Danadaa!! Vem minha odalisca, agora faz essa cobra coral subir!!! Pau que nasce torto, Nunca se endireita. Tchannn!! Tchannn!! Tu du du pááá! Eu gostchu muitchu, heinn! danadinha! Mainhaa! Agora use meu lorem ipsum ordinária!!! Olha o quibeee! rema, rema, ordinária!.
+              </p>
+              <a
+                href='/#contrate'
+                className='link-button'
+              >
+                Contrate!
+              </a>
+            </div>
+            <figure
+              className='phone'
+            >
+              <img
+                title='Lunnar Applications Apps'
+                alt='Lunnar Applications Apps'
+                className='img-fluid'
+                src={phoneSvg}
+              />
+            </figure>
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
