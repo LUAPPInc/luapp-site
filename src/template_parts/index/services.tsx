77cfa35b9eb42366b8ee1b6baf8af2a2e6bf37ed
Vinícius Marcili <vinipetter@gmail.com>
2019-09-28 19:48:36 -0300

    Services Page

--- src/template_parts/index/services.tsx
+++ src/template_parts/index/services.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 interface Props {}
 
@@ -8,7 +9,38 @@ const Services = ({}: Props) => {
       className='services'
       id='#servicos'
     >
-
+      <Container>
+        <Row>
+          <Col lg='12' className='align-self-start'>
+            <h2>
+              Serviços.
+              <span>
+                consultoria e desenvolvimento:
+              </span>
+            </h2>
+          </Col>
+          <Col lg='12' className='align-self-center'>
+            <h2>
+              Sistemas, Sites e Infraestruturas
+            </h2>
+          </Col>
+          <Col md='12' lg='4' className='align-self-end'>
+            <p>
+              Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai.
+            </p>
+          </Col>
+          <Col md='12' lg='4' className='align-self-end'>
+            <p>
+              Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai.
+            </p>
+          </Col>
+          <Col md='12' lg='4' className='align-self-end'>
+            <p>
+              Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
