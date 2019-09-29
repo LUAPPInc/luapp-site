28ebf3d76caf7ccff6045c3d8a8f3bc0e9afb812
Vinícius Marcili <vinipetter@gmail.com>
2019-09-28 21:07:46 -0300

    Contact

--- src/template_parts/index/contact.tsx
+++ src/template_parts/index/contact.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import TextField, {HelperText, Input} from '@material/react-text-field'
 
 interface Props {}
 
@@ -8,7 +10,76 @@ const Contact = ({}: Props) => {
       className='contact'
       id='#contato'
     >
-
+      <Container>
+        <Row>
+          <Col lg='12' className='align-self-start'>
+            <h2>
+              Contato
+            </h2>
+          </Col>
+          <Col lg='12' className='align-self-center'>
+            <form>
+              <div
+                className='wrapper-input'
+              >
+                <label>
+                  Nome
+                </label>
+                <input
+                  type='text'
+                  name='name'
+                  id='name'
+                  required
+                />
+              </div>
+              <div
+                className='wrapper-input slice'
+              >
+                <label>
+                  Email
+                </label>
+                <input
+                  type='text'
+                  name='email'
+                  id='email'
+                  required
+                />
+              </div>
+              <div
+                className='wrapper-input slice'
+              >
+                <label>
+                  Assunto
+                </label>
+                <input
+                  type='text'
+                  name='subject'
+                  id='subject'
+                />
+              </div>
+              <div
+                className='wrapper-input'
+              >
+                <label>
+                  Mensagem
+                </label>
+                <textarea
+                  name='message'
+                  id='message'
+                  required
+                ></textarea>
+              </div>
+              <div className='wrapper-submit'>
+                <button
+                  type='submit'
+                >
+                  Enviar
+                </button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
