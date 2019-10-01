8d54fcd1f51b855c7fa38a16f8def449914b39c1
Viní Marcili <vinipetter@gmail.com>
2019-10-01 16:53:41 -0300

    Update scripts

--- scripts/github.js
+++ scripts/github.js
@@ -4,7 +4,7 @@ const ghpages = require('gh-pages')
 ghpages.publish(
   'public',
   {
-    branch: 'source',
+    branch: 'master',
     repo: 'https://github.com/LUAPPInc/luappinc.github.io.git',
   },
   () => {
