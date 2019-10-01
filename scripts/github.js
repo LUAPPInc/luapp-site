cbd15af0a4ba9154d5c9dfc72b0de9ac82c0525a
Viní Marcili <vinipetter@gmail.com>
2019-10-01 17:04:03 -0300

    :sparkles:

--- scripts/github.js
+++ scripts/github.js
@@ -1,10 +1,10 @@
 const ghpages = require('gh-pages')
 
-ghpages.publish(
-  'public',
+ghpages.publish('public',
   {
     branch: 'master',
     repo: 'https://github.com/LUAPPInc/luappinc.github.io.git',
+    dest: './public'
   },
   () => {
     console.log('Deploy Complete!')
