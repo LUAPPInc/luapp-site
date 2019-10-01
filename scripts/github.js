8fb3dc259fd1a018cca57bc18b4a0188da1e9aea
Viní Marcili <vinipetter@gmail.com>
2019-10-01 17:09:31 -0300

    Remove public

--- scripts/github.js
+++ scripts/github.js
@@ -3,8 +3,7 @@ const ghpages = require('gh-pages')
 ghpages.publish('public',
   {
     branch: 'master',
-    repo: 'https://github.com/LUAPPInc/luappinc.github.io.git',
-    dest: './public'
+    repo: 'https://github.com/LUAPPInc/luappinc.github.io.git'
   },
   () => {
     console.log('Deploy Complete!')
