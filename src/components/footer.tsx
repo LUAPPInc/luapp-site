936ef6b4705fdf5898d56262da8a600b2a29c266
Viní Marcili <vinipetter@gmail.com>
2019-10-01 17:29:16 -0300

    Firebase Deploy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,6 +11,15 @@ const Footer = ({}: Props) => {
           &copy; {new Date().getFullYear()}. Todos os direitos reservados.
         </p>
       </Container>
+      {/* The core Firebase JS SDK is always required and must be listed first */}
+      <script src='/__/firebase/7.0.0/firebase-app.js' />
+
+      {/* TODO: Add SDKs for Firebase products that you want to use
+        https://firebase.google.com/docs/web/setup#available-libraries */}
+      <script src='/__/firebase/7.0.0/firebase-analytics.js' />
+
+      {/* Initialize Firebase */}
+      <script src='/__/firebase/init.js' />
     </footer>
   )
 }
