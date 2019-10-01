7f0d8fb850f3acb250da914c817109945b937dfc
Viní Marcili <vinipetter@gmail.com>
2019-10-01 15:54:34 -0300

    Deploy Github

---
+++ scripts/github.js
@@ -0,0 +1,12 @@
+const ghpages = require('gh-pages')
+
+ghpages.publish(
+  'public',
+  {
+    branch: 'master',
+    repo: 'https://github.com/LUAPPInc/luappinc.github.io.git',
+  },
+  () => {
+    console.log('Deploy Complete!')
+  }
+)
